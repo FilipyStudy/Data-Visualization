fc4fbb2a3fc77e45e7cf6179c532c9e80fdd0b63
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-05-23 21:42:23 -0300

    feat: moved files to another path to simplify the script writing

--- scripts/elt_script.py
+++ scripts/elt_script.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import subprocess
+import os
 
 SOURCE_DB = {
     'USER': 'postgres',
     'PASSWORD': 'secret',
     'HOST': 'destination_db'
 }
+
+csv_database = pd.read_csv(os.path.join("database", "weather_data.csv"))
+df_database = csv_database.to_sql()
+
+print(df_database.head())
