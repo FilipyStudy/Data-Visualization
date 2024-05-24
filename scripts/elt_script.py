ba667e7e117e520b98fcf750234bbee24a974e5d
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-05-24 20:49:54 -0300

    bug: tried to fix the 'file not found' error

--- scripts/elt_script.py
+++ scripts/elt_script.py
@@ -7,8 +7,15 @@ SOURCE_DB = {
     'PASSWORD': 'secret',
     'HOST': 'destination_db'
 }
+databasePath = "database"
 
-csv_database = pd.read_csv(os.path.join("database", "weather_data.csv"))
+if os.path.exists(databasePath):
+    with open (os.path.join(databasePath, "weather_data.csv")) as file:
+        csv_database = pd.read_csv(file)
+else:
+    print("The database path was not found.")
+    exit()
+
 df_database = csv_database.to_sql()
 
 print(df_database.head())
