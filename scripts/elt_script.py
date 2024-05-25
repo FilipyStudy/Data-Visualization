6bfb9e93cc692c1271f2d578a3e2ce287286aa9f
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-05-24 21:01:28 -0300

    bug: fixed error in not found file python error

--- scripts/elt_script.py
+++ scripts/elt_script.py
@@ -5,17 +5,19 @@ import os
 SOURCE_DB = {
     'USER': 'postgres',
     'PASSWORD': 'secret',
+    'HOST': 'source_db'
+}
+DESTINATION_DB = {
+    'USER': 'postgres',
+    'PASSWORD': 'secret',
     'HOST': 'destination_db'
 }
-databasePath = "database"
 
-if os.path.exists(databasePath):
-    with open (os.path.join(databasePath, "weather_data.csv")) as file:
+if os.path.exists("database"):
+    with open (os.path.join("database", "weather_data.csv")) as file:
         csv_database = pd.read_csv(file)
 else:
     print("The database path was not found.")
     exit()
 
-df_database = csv_database.to_sql()
-
-print(df_database.head())
+print(csv_database.head())
