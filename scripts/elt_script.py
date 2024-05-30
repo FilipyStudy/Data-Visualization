17d57b7e6bbbf195d81fa8e8258159096a1d081f
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-05-29 21:23:10 -0300

    bug: solved some bug's to output the error code of subprocess command

--- scripts/elt_script.py
+++ scripts/elt_script.py
@@ -12,23 +12,25 @@ DB = {
 
 #Check if the database service is alive to proceed, if not the function will trie 5 times until the process start.
 def check_db():
-    ping_db = subprocess.run(["mysqladmin" ,"ping", "-h", "localhost"],
+    ping_db = subprocess.run(["mysqladmin" ,"ping", f"-p{DB['PASSWORD']}"],
                              capture_output=True,
                              text=True)
-    print(f"First check: {ping_db.stdout()}")
-    if ('mysqld is alive' in ping_db.stdout()):
+    print(f"First check: {ping_db.stdout}")
+    print(f"First check error: {ping_db.stderr}")
+    if ('mysqld is alive' in ping_db.stdout):
         return True
     else:
         retries = 0
-        while ('mysqld is alive' not in ping_db.stdout() and retries <= 5):
+        while ('mysqld is alive' not in ping_db.stdout and retries <= 5):
             print(f"Retrying to see the running status of the database for the {retries + 1} time.")
             time.sleep(5) #Wait 5 seconds to check again.
-            ping_db = subprocess.run(['mysqladmin' ,'ping', '-h', 'localhost'], 
+            ping_db = subprocess.run(['mysqladmin' ,'ping', f"-p{DB['PASSWORD']}"], 
                         capture_output=True,
                         text=True)
             retries += 1
-            print(f"Retry output: {ping_db.stdout()}")
-        if ('mysqld is alive' in ping_db.stdout()):
+            print(f"Retry output: {ping_db.stdout}")
+            print(f"Retry output erro: {ping_db.stderr}")
+        if ('mysqld is alive' in ping_db.stdout):
             return True
         else:
             return False
