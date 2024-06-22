e484236a2a02f11af333779cb6bc6f708535a4cc
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-06-21 23:11:18 -0300

    feat: added some code to the etl script.

--- files/etl_script.py
+++ files/etl_script.py
@@ -2,7 +2,11 @@
 from connect_db import connect_db
 import pandas as pd
 from pathlib import Path
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import datetime
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 #Database access informations
 DB_CONFIG = {
@@ -11,6 +15,10 @@ DB_CONFIG = {
     'user': 'root',
     'user_pass': 'root'
 }
+
+#Create a date parser function 
+def dateparser(s):
+    return datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
 
 #Initialize the connection script
 connect_db = connect_db(host=DB_CONFIG['host'],
@@ -21,34 +29,34 @@ connect_db = connect_db(host=DB_CONFIG['host'],
 #Create a connection
 cnx = connect_db.connect()
 
+#Initialize the classifier of the algorithm
+clf = RandomForestClassifier(random_state=0)
+
 #Create a cursor to manipulate the database
 cursor = cnx.cursor()
 
 #Create engine for converting the csv file to a sql table.
 engine = connect_db.create_engine()
 
+#Defining a new limit to call the blocks
+mpl.rcParams['agg.path.chunksize'] = 10000
+
 #Open the csv file and pass it to a sql table
 with open(Path("databases/weather_data.csv")) as file:
-    csv_data = pd.read_csv(file)
+    #Read the CSV file and parse the date.
+    csv_data = pd.read_csv(file, parse_dates=['Date_Time'], date_parser=dateparser)
 
+    #Convert and upload to a MySQL database
     sql_data = csv_data.to_sql(name=f"{DB_CONFIG['database']}",
                                con=engine,
                                if_exists='replace',
                                chunksize=100)
+    
+    #Assign the sql table to a new variable.
+    sql_data = pd.read_sql_table(DB_CONFIG['database'], engine)
 
-#Read the database to a dataframe
-df = pd.read_sql(DB_CONFIG['database'],
-                 engine, 
-                 'Date_Time')
+#TODO:Convert the data into a numpy array object
+numpy_array = sql_data.to_numpy()
 
-rainfall = df['Precipitation_mm'].to_list()
-
-#Create a figure
-fig = plt.figure(figsize=(10.0, 7.0), dpi=100)
-
-ax = fig.add_axes([0,0,1,1])
-
-ax.plot(rainfall)
-
-#Show the graph
-plt.show()
+#Fit the data inside the model
+clf.fit(numpy_array[::1])
