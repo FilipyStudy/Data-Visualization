0cb922693293a9f1e8e404e0834a4ba7ac0719e4
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-07-08 15:49:40 -0300

    feat: added some tricks

--- files/etl_script.py
+++ files/etl_script.py
@@ -7,7 +7,7 @@ import matplotlib as mpl
 import datetime
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
 #Database access informations
 DB_CONFIG = {
     'host': 'db',
@@ -43,20 +43,26 @@ mpl.rcParams['agg.path.chunksize'] = 10000
 
 #Open the csv file and pass it to a sql table
 with open(Path("databases/weather_data.csv")) as file:
-    #Read the CSV file and parse the date.
+    #Read the file, using a function to parse the date to correct format.
     csv_data = pd.read_csv(file, parse_dates=['Date_Time'], date_parser=dateparser)
 
-    #Convert and upload to a MySQL database
+    #Convert the file to a sql database and upload the data.
     sql_data = csv_data.to_sql(name=f"{DB_CONFIG['database']}",
                                con=engine,
                                if_exists='replace',
                                chunksize=100)
-    
-    #Assign the sql table to a new variable.
-    sql_data = pd.read_sql_table(DB_CONFIG['database'], engine)
+    #Read the sql table
+    sql_data = pd.read_sql_table('test_db', engine)
 
-#TODO:Convert the data into a numpy array object
-numpy_array = sql_data.to_numpy()
+y = sql_data.iloc[:,3:]
+x = sql_data.iloc[:,4:]
 
-#Fit the data inside the model
-clf.fit(numpy_array[::1])
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_test, y_pred, color='blue', linewidth=2)
+plt.show()
