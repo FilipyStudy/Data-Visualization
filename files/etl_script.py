99affdccf0394287bec62a7b4637dba91e4ed8b7
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2024-07-08 18:33:54 -0300

    feat: finished the algorithm and the plot code

--- files/etl_script.py
+++ files/etl_script.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import datetime
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+
 #Database access informations
 DB_CONFIG = {
     'host': 'db',
@@ -16,9 +17,12 @@ DB_CONFIG = {
     'user_pass': 'root'
 }
 
-#Create a date parser function 
+#create a date parser function 
 def dateparser(s):
     return datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+
+#Initialize the classifier of the algorithm
+clf = LinearRegression()
 
 #Initialize the connection script
 connect_db = connect_db(host=DB_CONFIG['host'],
@@ -29,16 +33,12 @@ connect_db = connect_db(host=DB_CONFIG['host'],
 #Create a connection
 cnx = connect_db.connect()
 
-#Initialize the classifier of the algorithm
-clf = RandomForestClassifier(random_state=0)
-
 #Create a cursor to manipulate the database
 cursor = cnx.cursor()
 
 #Create engine for converting the csv file to a sql table.
 engine = connect_db.create_engine()
 
-#Defining a new limit to call the blocks
 mpl.rcParams['agg.path.chunksize'] = 10000
 
 #Open the csv file and pass it to a sql table
@@ -54,15 +54,20 @@ with open(Path("databases/weather_data.csv")) as file:
     #Read the sql table
     sql_data = pd.read_sql_table('test_db', engine)
 
-y = sql_data.iloc[:,3:]
-x = sql_data.iloc[:,4:]
+#Slice the data.
+y = np.array(sql_data.iloc[:,5])
+x = np.array(sql_data.loc[:,'Temperature_C':'Humidity_pct'])
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=0)
+#Split the data into train data and test data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
 
+#Fit the data inside the algorithm
 clf.fit(x_train, y_train)
 
+#Test the data
 y_pred = clf.predict(x_test)
 
-plt.scatter(x_test, y_test, color='red')
+#Plot and show the data for visualization purposes
+plt.scatter(x_test[:,0], y_test, color='red')
 plt.plot(x_test, y_pred, color='blue', linewidth=2)
 plt.show()
